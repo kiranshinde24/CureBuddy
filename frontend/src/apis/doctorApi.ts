3e0d1e5602c013e05ab55a3a1601a097e582c0b3
kiranshinde24 <137620704+kiranshinde24@users.noreply.github.com>
2025-07-05 17:23:55 +0530

    Update

--- frontend/src/apis/doctorApi.ts
+++ frontend/src/apis/doctorApi.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api"; // Replace with your backend URL
+const API_BASE_URL = import.meta.env.VITE_API_URL + "/api";
 
 export const fetchApprovedDoctors = async () => {
   try {
     const response = await axios.get(`${API_BASE_URL}/doctors/approved`);
-    return response.data; // returns an array
+    return response.data;
   } catch (error) {
     console.error("Error fetching approved doctors:", error);
     return [];
