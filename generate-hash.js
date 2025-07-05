66ff8a581b8e4c9946816c1ce3bfde62b644dcab
kiranshinde24 <137620704+kiranshinde24@users.noreply.github.com>
2025-07-05 12:10:14 +0530

    Delete generate-hash.js

--- generate-hash.js
+++
@@ -1,7 +0,0 @@
-// generate-hash.js
-const bcrypt = require('bcryptjs');
-
-const password = 'admin123'; // choose your desired admin password
-bcrypt.hash(password, 10).then((hash) => {
-  console.log(hash); // <-- This will be printed in your terminal
-});
