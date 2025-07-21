96268ff175aa6b64e38ab94cda5668c4b8202c9f
kiranshinde24 <137620704+kiranshinde24@users.noreply.github.com>
2025-07-21 23:10:44 +0530

    Update ResetPassword.tsx

--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -12,7 +12,7 @@ const ResetPassword = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        `http://localhost:5000/api/auth/reset-password/${id}/${token}`,
+       `${import.meta.env.VITE_API_URL}/api/auth/reset-password/${id}/${token}`,
         { password }
       );
       toast.success(res.data.message || "Password reset successfully");
